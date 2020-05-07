18bea00e8bdf526686324df8de24f9d7f46cad76
Edi Duluman <edi@duluman.ro>
2020-05-07 22:30:24 +0300

    Fixed App Component

--- src/App.js
+++ src/App.js
@@ -4,57 +4,49 @@ import Header from "./shared/header/Header.js";
 import { Grid, Container } from "@material-ui/core";
 import Search from "./components/search/Search.js";
 
-//   constructor(){
-//     super();
-//     this.state={
-// Movies: [],
-// searchField:""
-//     }
+  
+export default class App extends React.Component {
+
+  state={
+    Movies: [],
+    searchField:""
+  }
+
+  componentDidMount(){
+    fetch("https://api.themoviedb.org/3/movie/550?api_key=5cb751e64855ab4548a6d298c4270eea")
+    .then(response=>{
+      return response.json()
+    })
+    .then( data =>{
+      console.log(data)
+      this.setState({ Movies: [data] })
+    })
+  }
+
+
+  onSearchChange=event =>{
+    this.setState({searchField:event.target.value.toLowerCase()});
+  }
+ 
+  render() {
     
-//   }
-
-// componentDidMount(){
-// fetch("https://api.themoviedb.org/3/movie/550?api_key=5cb751e64855ab4548a6d298c4270eea")
-// .then(response=>{
-//   return response.json()
-// })
-// .then(Movies=>{
-//   this.setState({Movies:[...data.results]})
-// })
-
-// }
-
-
-//   onSearchChange=event =>{
-// this.setState({searchField:event.target.value.toLowerCase()});
-
-//   }
- 
-
-
-
-function App(){
-
-return (
-  <div className="App">
-  <Header />
-  <br />
-  <Grid container>
-    <Grid item xs={3} />
-    <Grid item xs={6}>
-      <Search  handleChange={this.onSearchChange}/>
-    </Grid>
-    <br /> <br /> <br />
-  </Grid>
-  <Container maxWidth="sm">
- 
-
-  </Container>
-</div>
-
-
-)
+    return (
+      <div className="App">
+        <Header />
+        <br />
+        <Grid container>
+          <Grid item xs={3} />
+          <Grid item xs={6}>
+            <Search  handleChange={this.onSearchChange}/>
+          </Grid>
+          <br /> <br /> <br />
+        </Grid>
+        <Container maxWidth="sm">
+      
+      
+        </Container>
+      </div>
+    )
+  }
 
 }
-
-export default App;
