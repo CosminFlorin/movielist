36b50878665cc293de21572dd2798b483759721c
CosminFlorin <mihalceacosminflorin@hotmail.com>
2020-06-13 13:56:22 +0300

    Updates

---
+++ precache-manifest.40e8c8c7de007b39dfe8e2fca3d44b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06754b93d0d974d431742f2c27633391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e962003f866e9fded5d",
+    "url": "/static/css/main.b9128479.chunk.css"
+  },
+  {
+    "revision": "9ccb0937d7fed3dcb321",
+    "url": "/static/js/2.45a073f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.45a073f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e962003f866e9fded5d",
+    "url": "/static/js/main.56ecca2d.chunk.js"
+  },
+  {
+    "revision": "799f9517bf74bdfe42d7",
+    "url": "/static/js/runtime-main.e1b60db5.js"
+  }
+]);
