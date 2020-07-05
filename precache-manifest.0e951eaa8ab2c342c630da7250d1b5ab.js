0a02d018296789b7773f81aad0244033b328d58e
CosminFlorin <mihalceacosminflorin@hotmail.com>
2020-07-05 15:54:37 +0300

    Updates

---
+++ precache-manifest.0e951eaa8ab2c342c630da7250d1b5ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826ee08449dd9e2543d5aaa8ef66d27a",
+    "url": "/movielist/index.html"
+  },
+  {
+    "revision": "66b0a51906360b80c879",
+    "url": "/movielist/static/css/main.b9128479.chunk.css"
+  },
+  {
+    "revision": "9ccb0937d7fed3dcb321",
+    "url": "/movielist/static/js/2.45a073f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movielist/static/js/2.45a073f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b0a51906360b80c879",
+    "url": "/movielist/static/js/main.a7197e72.chunk.js"
+  },
+  {
+    "revision": "2959e7116438a401605a",
+    "url": "/movielist/static/js/runtime-main.721fe295.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40e8c8c7de007b39dfe8e2fca3d44b77.js"
+  "/movielist/precache-manifest.0e951eaa8ab2c342c630da7250d1b5ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movielist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
